f095ee51b57d6b0b5c4a68581cd15a676743d1da
Robbe Hovaert <robbe@corbit.be>
2026-04-25 19:16:51 +0200

    initial project setupped

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import xhashAddon from 'xxhash-addon'
+const { XXHash3 } = xhashAddon
+import fs from 'fs'
+
+console.log('Hello, FileWarden!')
+const bufferedXHash3 = new XXHash3(Buffer.from([0, 0, 0, 0]))
+// get input buffer
+const stream = fs.createReadStream('./input/test.mkv')
+
+const startTime = Date.now()
+for await (const chunk of stream) {
+	bufferedXHash3.update(chunk)
+}
+const endTime = Date.now()
+
+const hash = bufferedXHash3.digest()
+console.log('Hash:', hash.toString('hex'))
+console.log('Hashing took', endTime - startTime, 'ms')
