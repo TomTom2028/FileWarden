c1a852368b7c290c5d5933007677973067007aa2
Robbe Hovaert <robbe.hovaert@gmail.com>
2026-05-15 22:34:45 +0200

    added EOF to quickhasher

--- src/hash/quickHasher.ts
+++ src/hash/quickHasher.ts
@@ -4,6 +4,7 @@ import { AbstractHasher } from './abstractHasher.ts'
 const CHUNK_SIZE = 1024 * 1024 // 1MB
 
 export default class QuickHasher extends AbstractHasher {
+
 	protected async windUpHasher(filePath: string) {
 		// take mtimeMS and size as preamble
 		const stats = await fs.stat(filePath)
@@ -16,15 +17,19 @@ export default class QuickHasher extends AbstractHasher {
 		// get a ponter to start of file
 		const fileHandle = await fs.open(filePath, 'r')
 		const buffer = Buffer.alloc(CHUNK_SIZE) // read first 1mb of the file
-		await fileHandle.read(buffer, 0, CHUNK_SIZE, 0)
-		this.updateHasher(buffer)
 
-		// every 10 mb, read another chunk until end of file
-		let offset = 10 * CHUNK_SIZE
+		// every 10 mb, read chunk until end of file
+		let offset = 0
 		while (offset < size) {
 			await fileHandle.read(buffer, 0, CHUNK_SIZE, offset)
 			this.updateHasher(buffer)
 			offset += 10 * CHUNK_SIZE
+		}
+		// also read last 1mb of the file to capture changes at the end of the file
+		if (size > CHUNK_SIZE) {
+			const lastChunkOffset = Math.max(0, size - CHUNK_SIZE)
+			await fileHandle.read(buffer, 0, CHUNK_SIZE, lastChunkOffset)
+			this.updateHasher(buffer)
 		}
 
 		await fileHandle.close()
